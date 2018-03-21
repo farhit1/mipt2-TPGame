d8002211937a570b989ddb1fec9c427a189b5265
farhit1 <32172892+farhit1@users.noreply.github.com>
2018-03-21 19:17:27 +0300

    Update availableUnits.cpp

--- availableUnits.cpp
+++ availableUnits.cpp
@@ -38,6 +38,7 @@ template<> const std::string UnitCRTP<Horseman>::_unitDesc = "Man on a horse";
 StoreUnit horsemanUnitStored(new Horseman());
 
 
+
 class Archer : public UnitCRTP<Archer> {
     using UnitCRTP<Archer>::UnitCRTP;
 };
@@ -54,3 +55,41 @@ template<> const int UnitCRTP<Archer>::_towerAttack = 40;
 template<> const std::string UnitCRTP<Archer>::_unitClass = "archer";
 template<> const std::string UnitCRTP<Archer>::_unitDesc = "Man with a bow";
 StoreUnit archerUnitStored(new Archer());
+
+
+
+class Cannon : public UnitCRTP<Cannon> {
+    using UnitCRTP<Cannon>::UnitCRTP;
+};
+
+template<> const int UnitCRTP<Cannon>::_coins = 500;
+template<> const int UnitCRTP<Cannon>::_people = 1;
+template<> const Resources* UnitCRTP<Cannon>::_resources = new Resources(std::vector<int>({0, 1}));
+template<> const int UnitCRTP<Cannon>::_dailyNeeds = 20;
+template<> const int UnitCRTP<Cannon>::_initialHealth = 400;
+template<> const int UnitCRTP<Cannon>::_outDamage = 50;
+template<> const int UnitCRTP<Cannon>::_inDamage = 25;
+template<> const float UnitCRTP<Cannon>::_moneylessDamage = 0.20;
+template<> const int UnitCRTP<Cannon>::_towerAttack = 100;
+template<> const std::string UnitCRTP<Cannon>::_unitClass = "cannon";
+template<> const std::string UnitCRTP<Cannon>::_unitDesc = "Really effective way to attack others' towers";
+StoreUnit cannonUnitStored(new Cannon());
+
+
+
+class Catapult : public UnitCRTP<Catapult> {
+    using UnitCRTP<Catapult>::UnitCRTP;
+};
+
+template<> const int UnitCRTP<Catapult>::_coins = 700;
+template<> const int UnitCRTP<Catapult>::_people = 2;
+template<> const Resources* UnitCRTP<Catapult>::_resources = new Resources(std::vector<int>({0, 0}));
+template<> const int UnitCRTP<Catapult>::_dailyNeeds = 100;
+template<> const int UnitCRTP<Catapult>::_initialHealth = 200;
+template<> const int UnitCRTP<Catapult>::_outDamage = 100;
+template<> const int UnitCRTP<Catapult>::_inDamage = 10;
+template<> const float UnitCRTP<Catapult>::_moneylessDamage = 2.0;
+template<> const int UnitCRTP<Catapult>::_towerAttack = 40;
+template<> const std::string UnitCRTP<Catapult>::_unitClass = "catapult";
+template<> const std::string UnitCRTP<Catapult>::_unitDesc = "Expensive, but good to destroy strong units";
+StoreUnit archerUnitStored(new Catapult());
